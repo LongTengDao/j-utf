5d69024d8c75672324c04e0d9cb2e026b3d2af4a
龙腾道 <LongTengDao@LongTengDao.com>
2017-12-15 02:07:55 +0800

    初始化导入

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# j-utf
+# `require('@ltd/j-utf')`
+
+## API
+
+### `.toStringFollowBOM(buffer[, swapDirectly])` 方法
+
+-   用途：根据传入 `Buffer` 的 `BOM` 值获取字符串（字符串中不包括 `BOM`）。
+-   参数：
+    *   `buffer`
+        -   用途：要解析为字符串的 `Buffer`。
+        -   类型：`Buffer`
+    +   `swapDirectly`
+        -   用途：当 `Buffer` 采用 `utf-16ge` 编码时，是否可以安全地在原始 `Buffer` 上进行位交换以减少开销。
+        -   类型：`boolean`
+        -   默认：`false`
+
+### `.toStringWithBOM(buffer[, swapDirectly])` 方法
+
+-   用途：根据传入 `Buffer` 的 `BOM` 值获取字符串（字符串中保留了 `BOM`）。
+-   参数：
+    *   `buffer`
+        -   用途：要解析为字符串的 `Buffer`。
+        -   类型：`Buffer`
+    +   `swapDirectly`
+        -   用途：当 `Buffer` 采用 `utf-16ge` 编码时，是否可以安全地在原始 `Buffer` 上进行位交换以减少开销。
+        -   类型：`boolean`
+        -   默认：`false`
+
+### `.BOM` 常量
+
+-   量值：`"\uFEFF"`
---
+++ src/lib/@.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { from } = Buffer;
+
+exports.toStringFollowBOM = (buffer,swapDirectly)=>{
+	switch( buffer[0] ){
+		case 0xEF: if( buffer[1]===0xBB && buffer[2]===0xBF ){
+			return buffer.slice(3).toString('utf8');
+		} break;
+		case 0xFF: if( buffer[1]===0xFE ){
+			return buffer.slice(2).toString('ucs2');
+		} break;
+		case 0xFE: if( buffer[1]===0xFF ){
+			buffer = ( swapDirectly ? buffer : from(buffer) ).swap16();
+			return buffer.slice(2).toString('ucs2');
+		} break;
+		default: return buffer.toString();
+	}
+};
+
+exports.toStringWithBOM = (buffer,swapDirectly)=>{
+	switch( buffer[0] ){
+		case 0xEF: if( buffer[1]===0xBB && buffer[2]===0xBF ){
+			return buffer.toString('utf8');
+		} break;
+		case 0xFF: if( buffer[1]===0xFE ){
+			return buffer.toString('ucs2');
+		} break;
+		case 0xFE: if( buffer[1]===0xFF ){
+			buffer = ( swapDirectly ? buffer : from(buffer) ).swap16();
+			return buffer.toString('ucs2');
+		} break;
+		default: return buffer.toString();
+	}
+};
+
+exports.BOM = '\uFEFF';
