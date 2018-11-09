d3908a90dc248d1830227bc7468a7ca63d1bdb51
龙腾道 <LongTengDao@GontemDo.com>
2018-11-09 09:41:35 +0800

    优化代码性能
    提前释放可能的内存空间。

--- dist/lib/@.js
+++ dist/lib/@.js
@@ -6,7 +6,7 @@ exports.toStringFollowBOM = (buffer,swapDirectly)=>{
 	switch( buffer[0] ){
 		case 0xEF: if( buffer[1]===0xBB && buffer[2]===0xBF ){ return buffer.slice(3).toString('utf8'); } break;
 		case 0xFF: if( buffer[1]===0xFE ){ return buffer.slice(2).toString('ucs2'); } break;
-		case 0xFE: if( buffer[1]===0xFF ){ buffer = ( swapDirectly ? buffer : from(buffer) ).swap16(); return buffer.slice(2).toString('ucs2'); } break;
+		case 0xFE: if( buffer[1]===0xFF ){ if ( !swapDirectly ) { buffer = from(buffer); } return buffer.swap16().slice(2).toString('ucs2'); } break;
 	}
 	return buffer.toString();
 };
@@ -15,7 +15,7 @@ exports.toStringWithBOM = (buffer,swapDirectly)=>{
 	switch( buffer[0] ){
 		case 0xEF: if( buffer[1]===0xBB && buffer[2]===0xBF ){ return buffer.toString('utf8'); } break;
 		case 0xFF: if( buffer[1]===0xFE ){ return buffer.toString('ucs2'); } break;
-		case 0xFE: if( buffer[1]===0xFF ){ buffer = ( swapDirectly ? buffer : from(buffer) ).swap16(); return buffer.toString('ucs2'); } break;
+		case 0xFE: if( buffer[1]===0xFF ){ if ( !swapDirectly ) { buffer = from(buffer); } return buffer.swap16().toString('ucs2'); } break;
 	}
 	return buffer.toString();
 };
